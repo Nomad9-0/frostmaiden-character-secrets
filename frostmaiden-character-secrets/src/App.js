3374077afb1c04e8cf998eb75a7dbf6585039603
Nomad9-0 <ojimenezlmpc@gmail.com>
2020-12-03 11:48:41 -0800

    basic layout created

---
+++ frostmaiden-character-secrets/src/App.js
@@ -0,0 +1,19 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Secrets from './components/Secrets.js';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+          <h1>Rime of the frostmaiden</h1>
+          <Secrets />
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ frostmaiden-character-secrets/src/components/Secrets.js
@@ -0,0 +1,13 @@
+import './Secrets.css';
+
+function Secrets() {
+  return (
+    <div className="Secrets">
+      <div className="cardsContainer">
+          <p>cards go here</p>
+      </div>
+    </div>
+  );
+}
+
+export default Secrets;
